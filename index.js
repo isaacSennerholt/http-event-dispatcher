088c8d0b5450cb72a702b9d28a6bcf49351f2e74
isaacSennerholt <isaac.sennerholt@gmail.com>
2019-05-21 17:26:27 +0200

    Update dispatch method params

--- index.js
+++ index.js
@@ -5,44 +5,45 @@ function assertConfigShape(config, requiredConfigurations = []) {
   if (!config) throw new Error('Event module configuration is missing.')
   return requiredConfigurations.forEach(requiredConfiguration => {
     const isConfigured = Object.keys(config).includes(requiredConfiguration)
-    if (!isConfigured) throw new Error(`"${requiredConfiguration}" is missing in configuration.`)
+    if (!isConfigured)
+      throw new Error(`"${requiredConfiguration}" is missing in configuration.`)
     return
   })
 }
 
 module.exports = config => {
   assertConfigShape(config, ['namespace', 'url', 'secret'])
-  const {namespace, url, secret} = config
+  const { namespace, url, secret } = config
 
-  function createEvent(type, payload) {
+  function createEvent(type, data) {
     return {
       type,
       created_at: Date.now(),
-      data: payload
+      data
     }
   }
 
   function createDigest(eventObject) {
     return crypto
-    .createHmac('sha256', secret)
-    .update(Buffer.from(JSON.stringify(eventObject)))
-    .digest('base64')
+      .createHmac('sha256', secret)
+      .update(Buffer.from(JSON.stringify(eventObject)))
+      .digest('base64')
   }
 
-  function dispatch(eventType, payload) {
-    const eventObject = createEvent(eventType, payload)
+  function dispatch(eventType, { headers = {}, data = {} }) {
+    const eventObject = createEvent(eventType, data)
     const digest = createDigest(eventObject)
     return httpRequest(url, {
       method: 'POST',
       headers: {
-        [`${namespace}-hmac-sha256`]: digest,
+        ...headers,
+        [`${namespace}-hmac-sha256`]: digest
       },
       body: eventObject
     })
   }
-  
+
   return {
     dispatch
   }
-  
 }
